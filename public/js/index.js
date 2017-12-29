a56624cb98f1b14ceb7f0eba4eb5a800d6ebb84b
Brian Iyoha <brian98ehis@gmail.com>
2017-12-29 14:34:01 +0100

    Add mustache.js for message templating

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,11 @@
 
 var socket = io();
 var location_btn = document.getElementById('location_btn');
-
+var message_template = document.getElementById('message-template').innerHTML; 
+var location_template = document.getElementById('location-template').innerHTML; 
 
 socket.on('connect', function () {
     console.log('connected');
-
-    // socket.emit('createMessage',{
-    //     to:"brian",
-    //     text:"socket"
-    // });
-
 });
 
 socket.on('disconnect', function () {
@@ -18,23 +13,26 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-    console.log("new email", message);
-    var li = document.createElement('ion-item');
-    li.textContent =  `${message.from}  ${moment(message.createdAt).format('hh:mm a')} :   ${message.text}`;
-
-    document.getElementById('receivedmsg').appendChild(li);
+    let html = Mustache.render(message_template,{
+        from:message.from,
+        text:message.text,
+        createdAt: `${ moment(message.createdAt).format('hh:mm a')}`
+    });
+ 
+       document.getElementById('receivedmsg').insertAdjacentHTML('beforeend',html);
 });
+
 socket.on('newLocation', function (data) {
-    var li = document.createElement('ion-item');
-    var link = document.createElement('a');
-    link.setAttribute('href', data.url);
-    link.setAttribute('target', "_blank");
-    link.textContent = "My Current Location";
-    li.innerText = `${data.from}  ${moment(data.createdAt).format('hh:mm a')} :`;
-    li.appendChild(link);
+    
+    let html = Mustache.render(location_template, {
+        from: data.from,
+        url: data.url,
+        createdAt: moment(data.createdAt).format('hh:mm a')
+    });
+
+    document.getElementById('receivedmsg').insertAdjacentHTML('beforeend',html);
 
 
-    document.getElementById('receivedmsg').appendChild(li);
     location_btn.innerHTML = ' <ion-icon name="pin" color="primary"></ion-icon>';
     location_btn.setAttribute('disabled', 'false');
 
@@ -65,13 +63,17 @@ location_btn.addEventListener('click', () => {
                 longitude
             });
 
-            console.log('position', pos);
         }, (e) => {
-            console.log('cannot get loac', e);
+
+            location_btn.innerHTML = ' <ion-icon name="pin" color="primary"></ion-icon>';
+            location_btn.setAttribute('disabled', 'false');
         });
         location_btn.setAttribute('disabled', 'true');
-        location_btn.innerHTML = '<p text-wrap>Sending Location</p>'
+        location_btn.innerHTML = '<p text-wrap>Sending Location</p>';
+        
     } else {
-        console.log('no location service');
+        location_btn.setAttribute('disabled', 'true');
+        location_btn.outerHTML = '<p text-wrap>cannot send location</p>';
+        
     }
 });
